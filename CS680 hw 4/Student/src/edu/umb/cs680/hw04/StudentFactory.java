9fe2ec48f0e29744dec3056e8ab27632d81896f1
ChandanaN99 <93791017+ChandanaN99@users.noreply.github.com>
2021-12-26 23:19:03 -0500

    Update StudentFactory.java

--- CS680 hw 4/Student/src/edu/umb/cs680/hw04/StudentFactory.java
+++ CS680 hw 4/Student/src/edu/umb/cs680/hw04/StudentFactory.java
@@ -4,25 +4,25 @@ public class StudentFactory {
 	
 	private StudentFactory() {}
 	
-	public static Student createInStateStudent(String name, String usAddr, int yrsInState) {
-		return new Student(name, usAddr, yrsInState, 0, null, StudentStatus.INSTATE);
+	public static Student createInStateStudent(String name, String usAddress, int yearsInState) {
+		return new Student(name, usAddress, yearsInState, 0, null, StudentStatus.INSTATE);
 	}
 	
-	public static Student createOutStateStudent(String name, String usAddr, int yrsInState) {
-		return new Student(name, usAddr, yrsInState, 0, null, StudentStatus.OUTSTATE);
+	public static Student createOutStateStudent(String name, String usAddress, int yearsInState) {
+		return new Student(name, usAddr, yearsInState, 0, null, StudentStatus.OUTSTATE);
 	}
 	
-	public static Student createIntlStudent(String name, String usAddr, int i20num, String foreignAddr) {
-		return new Student(name, usAddr, 0, i20num, foreignAddr, StudentStatus.INTL);
+	public static Student createIntlStudent(String name, String usAddress, int i20number, String foreignAddress) {
+		return new Student(name, usAddress, 0, i20number, foreignAddress, StudentStatus.INTL);
 	}
 	
 	/*public static void main(String[] args) {
-		Student s1 = createInStateStudent("Mark" , "Boston Downtown", 23);
+		Student s1 = createInStateStudent("Chandana" , "Dorchester", 22);
 		System.out.println("Name of Student: " + s1.getName());
-		System.out.println("Years in state: " + s1.getYrsInState());
-		System.out.println("US address of the student: " + s1.getUSAddr());
-		System.out.println("I-20 number: " + s1.geti20Num());
-		System.out.println("Foreign Address: " + s1.getForeignAddr());
+		System.out.println("Years in state: " + s1.getYearsInState());
+		System.out.println("US address: " + s1.getUSAddr());
+		System.out.println("I-20 number of the student: " + s1.geti20Number());
+		System.out.println("Foreign Address: " + s1.getForeignAddress());
 		System.out.println("Student status: " + s1.getStatus());
 		
 	}*/
